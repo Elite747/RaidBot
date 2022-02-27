4ef664c7918c665afa239f3b38c3213914f5e468
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2022-02-26 21:30:47 -0800

    Removed azure dependencies.

--- src/RaidBot/EventPersistence.cs
+++ src/RaidBot/EventPersistence.cs
@@ -1,44 +1,6 @@
-﻿using Azure.Identity;
-using Azure.Storage.Blobs;
-using System.Text.Json;
+﻿using System.Text.Json;
 
 namespace RaidBot;
-
-internal class AzurePersistence : IEventPersistence
-{
-    private readonly BlobContainerClient _client;
-
-    public AzurePersistence()
-    {
-        _client = new BlobServiceClient(new Uri("https://valhallalootliststorage.blob.core.windows.net/"), new DefaultAzureCredential())
-            .GetBlobContainerClient("raidbot");
-    }
-
-    public async Task<T?> LoadAsync<T>(ulong id) where T : class
-    {
-        var blob = _client.GetBlobClient($"{typeof(T).Name}-{id}.json");
-        if (await blob.ExistsAsync())
-        {
-            try
-            {
-                var stream = await blob.DownloadStreamingAsync();
-                return await JsonSerializer.DeserializeAsync<T>(stream.Value.Content);
-            }
-            catch
-            {
-            }
-        }
-        return null;
-    }
-
-    public async Task SaveAsync(ulong id, object content)
-    {
-        await using var ms = new MemoryStream();
-        await JsonSerializer.SerializeAsync(ms, content);
-        ms.Seek(0, SeekOrigin.Begin);
-        await _client.UploadBlobAsync($"{content.GetType().Name}-{id}.json", ms);
-    }
-}
 
 internal class LocalPersistence : IEventPersistence
 {
